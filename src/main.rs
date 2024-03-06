b5da182ca90f48845eaee79b71710768cb466459
Lars Rönnbäck <lars.ronnback@anchormodeling.com>
2024-03-06 23:06:42 +0100

    Starting on recursion

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::collections::HashMap;
 use fraction::Decimal;
 use rand::Rng;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Weightjoint {
     pub position: u8,
     pub content: Option<Vec<Weightjoint>>, 
@@ -13,14 +12,14 @@ pub struct Weightjoint {
 
 #[derive(Debug)]
 pub struct Hyperweight {
-    pub content: HashMap<usize, Vec<Weightjoint>>, 
+    pub content: Option<Vec<Weightjoint>>, 
     pub content_count: u32,
     pub accumulated_value: Decimal
 }
 impl Hyperweight {
     pub fn new() -> Self {
         Self {
-            content: HashMap::new(), 
+            content: Some(vec![Weightjoint {position: 0, content: None, content_value: Decimal::from(0), content_count: 0, accumulated_value: Decimal::from(0)}; 10]),
             content_count: 0,
             accumulated_value: Decimal::from(0)
         }
@@ -29,33 +28,24 @@ impl Hyperweight {
         let m = Decimal::from(u32::pow(10, position.into()));
         ((m * d).floor() % 10).try_into().unwrap()
     }
-    pub fn add_recurse(&mut self, Weightjoint: Weightjoint, position: u8, d: Decimal) {
-        
+    pub fn add_recurse(weightjoint: &mut Weightjoint, position: u8, weight: Decimal) {
+        if position < weight.get_precision() {
+            println!("{:?}", weightjoint);
+        }
     }
-    pub fn add(&mut self, d: Decimal) {
-        let mut point = Hyperweight::get_point(0, d);
-        if self.content.get(&point).is_none() {
-            self.content.insert(point, Vec::with_capacity(10));
-        }
-        let mut hypercontent = self.content.get_mut(&point).unwrap();
-        for i in 1..d.get_precision() {
-            point = Hyperweight::get_point(i, d);
-            if hypercontent.get(point).is_none() {
-                hypercontent[point] = Weightjoint {
-                    position: i, 
-                    content: None, 
-                    content_value: Decimal::from(0), 
-                    content_count: 0, 
-                    accumulated_value: Decimal::from(0)
-                }
-            }
-            hypercontent = hypercontent.get_mut(point).unwrap().content.unwrap();
-        }
+    pub fn add(&mut self, weight: Decimal) {
+        let point = Hyperweight::get_point(1, weight);
+        println!("Point: {}", point);
+        let weightjoint = self.content.as_mut().expect("Point is moot").get_mut(point).unwrap();
+        self.content_count = self.content_count + 1;
+        self.accumulated_value = self.accumulated_value + weight;
+        Hyperweight::add_recurse(weightjoint, 1, weight);
     }
 }
 
 fn main() {
-    let mut hb = Hyperweight::new();
-    hb.add(Decimal::from(9.12345));
-    println!("{:?}", hb);
+    let mut hw = Hyperweight::new();
+    hw.add(Decimal::from(0.54321));
+    println!("Count: {}", hw.content_count);
+    println!("Accum: {}", hw.accumulated_value);
 }
